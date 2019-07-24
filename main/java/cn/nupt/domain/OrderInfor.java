27c5bafcfd9829a676cdbee3b136ad20620093f4
pingan <pizan@foxmail.com>
2019-07-24 21:45:52 +0800

    创建数据库对应的实体类

---
+++ main/java/cn/nupt/domain/OrderInfor.java
@@ -0,0 +1,11 @@
+package cn.nupt.domain;
+
+/**
+ * 订单信息
+ *
+ * @AUTHOR PizAn
+ * @CREAET 2019-07-24 21:34
+ */
+
+public class OrderInfor {
+}
---
+++ main/java/cn/nupt/domain/SeckGoods.java
@@ -0,0 +1,57 @@
+package cn.nupt.domain;
+
+import java.util.Date;
+
+/**
+ * 参与秒杀的商品类
+ *
+ * @AUTHOR PizAn
+ * @CREAET 2019-07-24 21:32
+ */
+
+public class SeckGoods {
+    private Long id;
+    private Long goodsId;
+    private Double miaoshaPrice;
+    private Integer stockCount;
+    private Date startDate;
+    private Date endDate;
+
+    public Double getMiaoshaPrice() {
+        return miaoshaPrice;
+    }
+    public void setMiaoshaPrice(Double miaoshaPrice) {
+        this.miaoshaPrice = miaoshaPrice;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getGoodsId() {
+        return goodsId;
+    }
+    public void setGoodsId(Long goodsId) {
+        this.goodsId = goodsId;
+    }
+    public Integer getStockCount() {
+        return stockCount;
+    }
+    public void setStockCount(Integer stockCount) {
+        this.stockCount = stockCount;
+    }
+    public Date getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+}
